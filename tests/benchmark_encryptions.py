f34c3bb865e216ddc11a40700cac6e2f13dfa742
WUAmin <wuamin@gmail.com>
2020-03-24 23:20:12 +0100

    Encryption benchmark using functions in helper.py

--- tests/benchmark_encryptions.py
+++ tests/benchmark_encryptions.py
@@ -2,7 +2,8 @@ import base64
 import os
 import time
 
-from wublackhole.helper import create_random_content_file, get_checksum_sha256_file, sizeof_fmt
+from wublackhole.helper import chacha20poly1305_encrypt_file, create_random_content_file, get_checksum_sha256_file, \
+    sizeof_fmt, chacha20poly1305_decrypt_file
 
 
 # Config
@@ -10,6 +11,7 @@ test_filename = "checksum_file.tmp"
 test_filepath = os.path.join(os.path.split(__file__)[0], test_filename)
 test_file_size = 48 * 1024 * 1024  # 48MB
 password_provided = "8mwHncKVXalaBAIe"  # This is input in the form of a string
+password = password_provided.encode()  # Convert to type bytes
 
 # Create Test file
 start_t = time.process_time()
@@ -17,6 +19,11 @@ create_random_content_file(test_filepath, test_file_size)
 elapsed_t = time.process_time() - start_t
 print("Create a {} test file in {:06f} secs...".format(sizeof_fmt(test_file_size), elapsed_t))
 test_file_checksum = get_checksum_sha256_file(test_filename)
+
+
+
+
+
 
 # ====  hazmat/Fernet ====
 from cryptography.fernet import Fernet
@@ -27,7 +34,6 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 encrypted_test_filepath = test_filename + ".hazmat-fernet"
 decrypted_test_filepath = test_filename + ".hazmat-fernet.decrypt"
-password = password_provided.encode()  # Convert to type bytes
 # salt = b'salt_' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
 salt = os.urandom(16)
 kdf = PBKDF2HMAC(
@@ -58,6 +64,39 @@ with open(decrypted_test_filepath, 'wb') as f:
     f.write(encrypted)
 elapsed_t = time.process_time() - start_t
 print("Decrypt - hazmat/Fernet:     {:06f} secs...".format(elapsed_t))
+decrypt_file_checksum = get_checksum_sha256_file(decrypted_test_filepath)
+if test_file_checksum == decrypt_file_checksum:
+    print("Checksum matches.")
+else:
+    print("ERROR: Mismatch checksum after encrypt/decrypt.")
+os.remove(encrypted_test_filepath)
+os.remove(decrypted_test_filepath)
+print("\n\n")
+
+
+# ==== hazmat/ChaCha20Poly1305 implemented on helper.py ====
+encrypted_test_filepath = test_filename + ".ChaCha20Poly1305"
+decrypted_test_filepath = test_filename + ".ChaCha20Poly1305.decrypt"
+start_t = time.process_time()
+key, nonce = chacha20poly1305_encrypt_file(raw_filepath=test_filepath,
+                                           encrypted_filepath=encrypted_test_filepath,
+                                           secret=password_provided.encode())
+elapsed_t = time.process_time() - start_t
+print("Encrypt - hazmat/ChaCha20Poly1305:     {:06f} secs...".format(elapsed_t))
+
+start_t = time.process_time()
+key_str = key.hex()
+nonce_str = nonce.hex()
+print(key_str, nonce_str)
+key2 = bytes.fromhex(key_str)
+nonce2 = bytes.fromhex(nonce_str)
+chacha20poly1305_decrypt_file(encrypted_filepath=encrypted_test_filepath,
+                              decrypted_filepath=decrypted_test_filepath,
+                              secret=password_provided.encode(),
+                              key=key2,
+                              nonce=nonce2)
+elapsed_t = time.process_time() - start_t
+print("Decrypt - hazmat/ChaCha20Poly1305:     {:06f} secs...".format(elapsed_t))
 decrypt_file_checksum = get_checksum_sha256_file(decrypted_test_filepath)
 if test_file_checksum == decrypt_file_checksum:
     print("Checksum matches.")
@@ -140,3 +179,5 @@ elapsed_t = time.process_time() - start_t
 print("Encrypt - AES:     {:06f} secs...".format(elapsed_t))
 os.remove(encrypted_test_filepath)
 # os.remove(decrypted_test_filepath)
+
+os.remove(test_filepath)
