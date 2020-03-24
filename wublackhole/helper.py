9de8e7949d76aa85f4f979cebca05410177baaa7
WUAmin <wuamin@gmail.com>
2020-03-24 23:21:31 +0100

    chacha20poly1305 encrypt/decrypt and compress/decompress bytes_to_string_b64zlib

--- wublackhole/helper.py
+++ wublackhole/helper.py
@@ -1,6 +1,10 @@
 import hashlib
 import logging
 import os
+import zlib
+from base64 import b64decode, b64encode
+
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 
 def sizeof_fmt(num: int, trailing_zeros: int = 2, suffix: str = 'B', separate_prefix: bool = True) -> str:
@@ -75,5 +79,54 @@ def get_checksum_sha256_folder(dirpath: str, block_size: int = 16384, running_ha
     return running_hash.hexdigest()
 
 
-def encrypt_file(filepath: str, secret: str):
-    pass
+def chacha20poly1305_encrypt_data(data: bytes, secret: bytes, key: bytes = None, nonce: bytes = None):
+    """
+    Encrypt data using secret, key and nonce. key and nonce can be None, in that case they will be generated and
+    can be obtained in return.
+    :param data:
+    :param secret:
+    :param key:
+    :param nonce:
+    :return: tulip of (encrypted_data, key, nonce)
+    """
+    if key is None:
+        key = ChaCha20Poly1305.generate_key()
+    if nonce is None:
+        nonce = os.urandom(12)
+    chacha = ChaCha20Poly1305(key)
+    return chacha.encrypt(nonce, data, secret), key, nonce
+
+
+def chacha20poly1305_encrypt_file(raw_filepath: str, encrypted_filepath: str, secret: bytes, key: bytes = None,
+                                  nonce: bytes = None):
+    with open(raw_filepath, 'rb') as f_r:
+        with open(encrypted_filepath, 'wb') as f_w:
+            data = f_r.read()
+            encrypted, key, nonce = chacha20poly1305_encrypt_data(data=data, secret=secret, key=key, nonce=nonce)
+            f_w.write(encrypted)
+            return key, nonce
+    return None, None
+
+
+def chacha20poly1305_decrypt_data(data: bytes, secret: bytes, key: bytes, nonce: bytes):
+    chacha = ChaCha20Poly1305(key)
+    return chacha.decrypt(nonce, data, secret)
+
+
+def chacha20poly1305_decrypt_file(encrypted_filepath: str, decrypted_filepath: str, secret: bytes, key: bytes,
+                                  nonce: bytes):
+    with open(encrypted_filepath, 'rb') as f_r:
+        with open(decrypted_filepath, 'wb') as f_w:
+            data = f_r.read()
+            decrypted = chacha20poly1305_decrypt_data(data=data, secret=secret, key=key, nonce=nonce)
+            f_w.write(decrypted)
+            return len(decrypted)
+    return None
+
+
+def compress_bytes_to_string_b64zlib(data: bytes) -> str:
+    return b64encode(zlib.compress(data)).decode('ascii')
+
+
+def decompress_bytes_to_string_b64zlib(string: str) -> str:
+    return zlib.decompress(b64decode(string.encode('ascii')))
