dca80963ebb80f5a3abffb551ffba583ffc6d51b
WUAmin <wuamin@gmail.com>
2020-03-31 13:11:07 +0200

    Client: Separate parse arguments from setup and load app for using in test

--- wublackholeclient.py
+++ wublackholeclient.py
@@ -11,7 +11,18 @@ from pyclient.client_config import client
 from pyclient.main_window import ClientMainWindow
 
 
-def init_confg_dir():
+def setup_client():
+    if os.path.exists(client.config_filepath):  # Check if config file exist
+        client.load()  # load config
+    else:
+        client.init_config()
+    # Setup Database
+    client.init_database()
+    # Setup Bot
+    client.init_bot(client.client['bot']['api'], client.client['bot']['proxy'])
+
+
+def parse_args():
     # Parse application arguments
     parser = argparse.ArgumentParser(description='Send everything to WU-BlackHole using Telegram Bot')
     parser.add_argument('--config', '-c',
@@ -25,24 +36,11 @@ def init_confg_dir():
         client.config_filepath = os.path.join("config", "client_config.json")
         os.makedirs("config", exist_ok=True)
 
-    if os.path.exists(client.config_filepath):  # Check if config file exist
-        client.load()  # load config
-    else:
-        client.init_config()
-    # Setup Database
-    client.init_database()
-    # Setup Bot
-    client.init_bot(client.client['bot']['api'], client.client['bot']['proxy'])
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    init_confg_dir()
+    parse_args()
+    setup_client()
     main_window = ClientMainWindow()
     # Show Window
     main_window.window.show()
